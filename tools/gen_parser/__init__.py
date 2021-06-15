9f6920642b6b7c5548420c0c2b04d75754b89676
Stu Cork <stu@anvil.works>
2021-06-15 13:06:08 +0800

    fix name error from gen_parser rename

--- tools/gen_parser/__init__.py
+++ tools/gen_parser/__init__.py
@@ -7,5 +7,5 @@ import sys
 path_to_cpython = "../cpython"
 sys.path += [path_to_cpython + "/Tools/peg_generator"]
 
-js_generator = "tools/build_parser/skulpt_parser_genarator.py"
+js_generator = "tools/gen_parser/skulpt_parser_genarator.py"
 out_file = "src/parser/generated_parser.ts"
