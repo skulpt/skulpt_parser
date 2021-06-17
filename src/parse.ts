a2ecab09428a268c502e46b72c4e8c06f3551b75
Stu Cork <stu@anvil.works>
2021-06-17 12:07:57 +0800

    small tweaks to parse.ts

--- src/parse.ts
+++ src/parse.ts
@@ -5,13 +5,12 @@ import { dump } from "./ast/ast.ts";
 
 import { readLines } from "https://deno.land/std@0.99.0/io/mod.ts";
 import { parse } from "https://deno.land/std@0.99.0/flags/mod.ts";
-import * as path from "https://deno.land/std@0.99.0/path/mod.ts";
 
 const args = parse(Deno.args);
 
 console.assert(args._.length == 1, "Must pass filename as argument");
 
-const filename = path.join(Deno.cwd(), args._[0] as string);
+const filename = args._[0] as string;
 
 const fileReader = await Deno.open(filename);
 
@@ -23,13 +22,7 @@ for await (const line of readLines(fileReader)) {
 
 fileReader.close();
 
-function* lineProducer(lines: string[]): IterableIterator<string> {
-    for (const line of lines) {
-        yield line;
-    }
-}
-
-const tokens = tokenize(lineProducer(lines));
+const tokens = tokenize(lines[Symbol.iterator]());
 
 const tokenizer = new Tokenizer(tokens);
 
