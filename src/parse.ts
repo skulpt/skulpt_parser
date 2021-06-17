794826e56a89dd87dfb99d3d147cbc58e7bc87eb
Stu Cork <stu@anvil.works>
2021-06-17 12:30:23 +0800

    create a syncronous readFile line generator

--- src/parse.ts
+++ src/parse.ts
@@ -1,9 +1,9 @@
 import { GeneratedParser } from "./parser/generated_parser.ts";
 import { Tokenizer } from "./tokenize/Tokenizer.ts";
 import { tokenize } from "./tokenize/tokenize.ts";
+import { readFile } from "./tokenize/readline.ts";
 import { dump } from "./ast/ast.ts";
 
-import { readLines } from "https://deno.land/std@0.99.0/io/mod.ts";
 import { parse } from "https://deno.land/std@0.99.0/flags/mod.ts";
 
 const args = parse(Deno.args);
@@ -12,17 +12,7 @@ console.assert(args._.length == 1, "Must pass filename as argument");
 
 const filename = args._[0] as string;
 
-const fileReader = await Deno.open(filename);
-
-const lines = [];
-
-for await (const line of readLines(fileReader)) {
-    lines.push(line);
-}
-
-fileReader.close();
-
-const tokens = tokenize(lines[Symbol.iterator]());
+const tokens = tokenize(readFile(filename));
 
 const tokenizer = new Tokenizer(tokens);
 
--- src/tokenize/readline.ts
+++ src/tokenize/readline.ts
@@ -6,3 +6,9 @@ export function* readString(text: string): IterableIterator<string> {
         yield match[0] + "\n";
     }
 }
+
+/** yields lines from a file given a filename */
+export function readFile(filename: string): IterableIterator<string> {
+    const text = Deno.readTextFileSync(filename);
+    return readString(text);
+}
