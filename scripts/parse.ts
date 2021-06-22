a12c6abfc83cfba7f1818afd3d2d119ae46edf33
Stu Cork <stu@anvil.works>
2021-06-22 22:18:16 +0800

    improve parse script

--- scripts/parse.ts
+++ scripts/parse.ts
@@ -4,8 +4,14 @@ import { getDiff } from "../support/diff.ts";
 import { getPyAstDump } from "../support/py_ast_dump.ts";
 import { runParserFromFile } from "../src/parser/parse.ts";
 
-const { _: args, mode, no_compare } = parse(Deno.args, { default: { mode: "exec" }, boolean: ["no_compare"] });
-const compare = !no_compare;
+const argv = parse(Deno.args, {
+    default: { mode: "exec" } /** @todo this doesn't get passed to the python script */,
+    boolean: ["no_compare"],
+    alias: { mode: "m", no_compare: "nc" },
+});
+
+const { _: args, mode, no_compare: noCompare } = argv;
+
 const options = { indent: 2, include_attributes: true };
 
 console.assert(args.length == 1, Colors.bold(Colors.bgRed(Colors.white(" Must pass filename as argument "))));
@@ -19,36 +25,24 @@ if (typeof filearg === "number") {
     filename = filearg;
 }
 
-let pyDump = "";
-
-if (compare) {
-    console.log();
-    console.log(Colors.bold(Colors.magenta("##### py #####")));
-    pyDump = await getPyAstDump(Deno.readTextFileSync(filename), options);
-    console.log(Colors.magenta(pyDump));
-    console.log();
-}
-
-let jsDump = "";
 const ast = runParserFromFile(filename, mode);
-if (ast !== null) {
-    try {
-        jsDump = dump(ast, options);
-        if (compare) {
-            console.log(Colors.bold(Colors.green("/**** js ****/")));
-            console.log(Colors.green(jsDump));
-        } else {
-            console.log(jsDump);
-        }
-    } catch {
-        console.log("ast dump failed - JSON version");
-        console.log(Colors.yellow(JSON.stringify(ast, null, 2)));
-    }
-} else {
-    throw new Error("Parser returned null");
+if (ast === null) {
+    throw new Error(Colors.red("Parser returned null"));
 }
 
-if (compare) {
-    console.log();
-    console.log(getDiff(jsDump, pyDump));
+const jsDump = dump(ast, options);
+
+if (noCompare) {
+    console.log(jsDump);
+    Deno.exit();
 }
+
+console.log();
+console.log(Colors.bold(Colors.magenta("##### py (EXPECTED) #####")));
+const pyDump = await getPyAstDump(Deno.readTextFileSync(filename), options);
+console.log(Colors.magenta(pyDump));
+console.log();
+console.log(Colors.bold(Colors.green("/**** js  (ACTUAL)   ****/")));
+console.log(Colors.green(jsDump));
+console.log();
+console.log(getDiff(jsDump, pyDump));
