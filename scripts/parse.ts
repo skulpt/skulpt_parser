3984a4673deeebb44221cc3c3443204b1c6b65f0
Stu Cork <stu@anvil.works>
2021-06-22 21:47:09 +0800

    implement test_peg_parser from cpython

--- scripts/parse.ts
+++ scripts/parse.ts
@@ -5,6 +5,7 @@ import { getPyAstDump } from "../support/py_ast_dump.ts";
 import { runParserFromFile } from "../src/parser/parse.ts";
 
 const args = parse(Deno.args);
+const mode = args.mode || "exec";
 const options = { indent: 2, include_attributes: true };
 
 console.assert(args._.length == 1, Colors.bold(Colors.bgRed(Colors.white(" Must pass filename as argument "))));
@@ -18,26 +19,38 @@ if (typeof filearg === "number") {
     filename = filearg;
 }
 
-console.log();
-console.log(Colors.bold(Colors.magenta("##### py #####")));
-const pyDump = await getPyAstDump(Deno.readTextFileSync(filename), options);
-console.log(Colors.magenta(pyDump));
-console.log();
+const compare = !args["no_compare"];
+
+let pyDump = "";
+
+if (compare) {
+    console.log();
+    console.log(Colors.bold(Colors.magenta("##### py #####")));
+    pyDump = await getPyAstDump(Deno.readTextFileSync(filename), options);
+    console.log(Colors.magenta(pyDump));
+    console.log();
+}
 
 let jsDump = "";
-const ast = runParserFromFile(filename);
+const ast = runParserFromFile(filename, mode);
 if (ast !== null) {
     try {
         jsDump = dump(ast, options);
-        console.log(Colors.bold(Colors.green("/**** js ****/")));
-        console.log(Colors.green(jsDump));
+        if (compare) {
+            console.log(Colors.bold(Colors.green("/**** js ****/")));
+            console.log(Colors.green(jsDump));
+        } else {
+            console.log(jsDump);
+        }
     } catch {
         console.log("ast dump failed - JSON version");
-        console.log(Colors.green(JSON.stringify(ast, null, 2)));
+        console.log(Colors.yellow(JSON.stringify(ast, null, 2)));
     }
 } else {
-    console.error("parser returned null");
+    throw new Error("Parser returned null");
 }
 
-console.log();
-console.log(getDiff(jsDump, pyDump));
+if (compare) {
+    console.log();
+    console.log(getDiff(jsDump, pyDump));
+}
