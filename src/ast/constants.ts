aba59651365b44c54174419e0f1f6eabe35a4c74
Stu Cork <stu@anvil.works>
2021-06-26 10:50:44 +0800

    use Symbol.toStringTag as a possible way to distinguish types when moving to skulpt

--- src/ast/constants.ts
+++ src/ast/constants.ts
@@ -1,6 +1,7 @@
 import { assert } from "../parser/pegen.ts";
 
 export class pyConstant<V> {
+    static _name = "constant";
     _v: V;
     constructor(v: V) {
         this._v = v;
@@ -11,11 +12,15 @@ export class pyConstant<V> {
     valueOf(): V {
         return this._v;
     }
+    get [Symbol.toStringTag]() {
+        return (this.constructor as typeof pyConstant)._name;
+    }
 }
 
 export class pyStr extends pyConstant<string> {
+    static _name = "str";
     toString() {
-        const v = this._v;
+        const v = this._v.replace(/\n/g, "\\n");
         let quote = "'";
         if (v.includes("'") && !v.includes('"')) {
             quote = '"';
@@ -25,9 +30,12 @@ export class pyStr extends pyConstant<string> {
 }
 
 /** @todo we cant really have bigint here - we could use JSBI or string instead */
-export class pyInt extends pyConstant<number | bigint> {}
+export class pyInt extends pyConstant<number | bigint> {
+    static _name = "int";
+}
 
 export class pyFloat extends pyConstant<number> {
+    static _name = "float";
     /** @todo - make this string a little more involved */
     toString() {
         const v = this._v;
@@ -41,36 +49,46 @@ export class pyFloat extends pyConstant<number> {
 }
 
 export class pyComplex extends pyConstant<{ real: number; imag: number }> {
-    constructor(c: { real?: number; imag?: number }) {
-        const real = c.real ?? 0.0;
-        const imag = c.imag ?? 0.0;
+    static _name = "complex";
+    constructor({ real = 0, imag }: { real?: number; imag: number }) {
+        // this is from the tokenizer and we shouldn't have a real except 0.
+        assert(real === 0);
         super({ real, imag });
     }
     toString() {
-        const { real, imag } = this._v;
-        // this is from the tokenizer and we shouldn't have a real except 0.
-        assert(real === 0);
+        const { imag } = this._v;
         return imag + "j";
     }
 }
 
 export class pyBytes extends pyConstant<Uint8Array> {
-    /** @todo toString() */
+    static _name = "bytes";
+    toString() {
+        const v = new TextDecoder().decode(this._v).replace(/\n/g, "\\n");
+        let quote = "'";
+        if (v.includes("'") && !v.includes('"')) {
+            quote = '"';
+        }
+        return "b" + quote + v + quote;
+    }
 }
 
 export class pyNoneType extends pyConstant<null> {
+    static _name = "NoneType";
     toString() {
         return "None";
     }
 }
 
 export class pyBool extends pyConstant<boolean> {
+    static _name = "bool";
     toString() {
         return this._v ? "True" : "False";
     }
 }
 
 export class pyEllipsisType extends pyConstant<"..."> {
+    static _name = "ellipsis";
     toString() {
         return "Ellipsis";
     }
