ce86c016024b8ce7b0e727d0073d01a79ecdc7a3
Stu Cork <stu@anvil.works>
2021-06-30 14:14:00 +0800

    improve performance of cache

--- src/tokenize/Tokenizer.ts
+++ src/tokenize/Tokenizer.ts
@@ -1,3 +1,4 @@
+import type { AST } from "../ast/astnodes.ts";
 import { isSpace } from "../util/str_helpers.ts";
 import { COMMENT, ERRORTOKEN, NL } from "./token.ts";
 import type { TokenInfo } from "./tokenize.ts";
@@ -5,19 +6,22 @@ import type { TokenInfo } from "./tokenize.ts";
 export class Tokenizer {
     _tokengen: Iterator<TokenInfo, TokenInfo>;
     _tokens: TokenInfo[];
+    _cache: { [key: string]: [AST | TokenInfo | null, number] }[];
     _index: number;
     constructor(tokengen: Iterator<TokenInfo, TokenInfo>) {
         this._tokengen = tokengen;
         this._tokens = [];
+        this._cache = [{}];
         this._index = 0;
     }
     _push(): void {
         while (this._index === this._tokens.length) {
             const tok = this._tokengen.next().value;
-            if (tok.type === NL || tok.type === COMMENT) {
+            const type = tok.type;
+            if (type === NL || type === COMMENT) {
                 continue;
             }
-            if (tok.type === ERRORTOKEN && isSpace(tok.string)) {
+            if (type === ERRORTOKEN && isSpace(tok.string)) {
                 continue;
             }
             this._tokens.push(tok);
@@ -25,6 +29,7 @@ export class Tokenizer {
     }
     getnext(): TokenInfo {
         this._push();
+        this._cache.push({});
         return this._tokens[this._index++];
     }
     peek(): TokenInfo {
