19263f387782f77d0b12890e10fb17d0c0ba4dd7
stu <stu@anvil.works>
2023-03-14 22:46:18 +0800

    update scripts use deno.json instead of vr

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-export { parse } from "https://deno.land/std@0.99.0/flags/mod.ts";
-export * as Colors from "https://deno.land/std@0.99.0/fmt/colors.ts";
-export { assertEquals, assertThrows, assert } from "https://deno.land/std@0.99.0/testing/asserts.ts";
-export * as esbuild from "https://deno.land/x/esbuild@v0.11.10/mod.js";
-export { AssertionError } from "https://deno.land/std@0.99.0/testing/asserts.ts";
-export { bench, runBenchmarks } from "https://deno.land/std@0.99.0/testing/bench.ts";
+export { parse } from "https://deno.land/std@0.179.0/flags/mod.ts";
+export * as Colors from "https://deno.land/std@0.179.0/fmt/colors.ts";
+export { assertEquals, assertThrows, assert } from "https://deno.land/std@0.179.0/testing/asserts.ts";
+export * as esbuild from "https://deno.land/x/esbuild@v0.17.11/mod.js";
+export { AssertionError } from "https://deno.land/std@0.179.0/testing/asserts.ts";
+export { createRequire } from "https://deno.land/std@0.177.0/node/module.ts";
